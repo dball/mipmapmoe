c2aa6d4344eef4d09df811733289631ead98b474
Donald Ball <donald.ball@livingsocial.com>
2015-08-14 20:54:00 -0400

    Switch context to grid scale, rewrite drag fn in closure

--- src/db/mmm.cljs
+++ src/db/mmm.cljs
@@ -10,75 +10,77 @@
        (finally
          (canvas/restore ~context)))))
 
-(enable-console-print!)
-
 (def scene
   (atom {}))
 
 (defn draw!
   []
-  (let [{:keys [canvas]} @scene
-        height (.-height canvas)
+  (let [{:keys [canvas center scale]} @scene
         width (.-width canvas)
+        height (.-height canvas)
         context (canvas/get-context canvas "2d")]
-    (loop [x 0.5]
-      (when (< x width)
-        (-> context
-            (canvas/move-to x 0)
-            (canvas/line-to x height))
-        (recur (+ x 10))))
-    (loop [y 0.5]
-      (when (< y height)
-        (-> context
-            (canvas/move-to 0 y)
-            (canvas/line-to width y))
-        (recur (+ y 10))))
-    (-> context
-        (canvas/stroke-style "#eee")
-        (canvas/stroke))))
+    (println "draw!" scale)
+    (with-style context
+      (canvas/transform context scale 0 0 scale (/ width 2) (/ height 2))
+      (canvas/stroke-style context "#eee")
+      (canvas/stroke-width context 0.1)
+      (doseq [radius (range 1 50)]
+        (canvas/circle context {:x 0 :y 0 :r radius})
+        (canvas/stroke context)))))
 
 (defn position
   [event]
   [(.-clientX event) (.-clientY event)])
 
+(defn parse-pixels
+  [s]
+  (when-let [[_  pixels] (re-find #"(-?\d+)px" s)]
+    pixels))
+
 (defn mouse-down
   [event]
-  (let [{:keys [canvas]} @scene]
+  (let [{:keys [canvas]} @scene
+        canvas-offset (mapv parse-pixels [(.-left (.-style canvas))
+                                          (.-top (.-style canvas))])
+        drag-offset (position event)
+        offset (mapv - canvas-offset drag-offset)]
     (set! (.-cursor (.-style canvas)) "move")
     (swap! scene assoc
-           :drag (mapv (partial * -1) (position event)))))
+           :drag-fn (fn [event]
+                      (set! (.-cursor (.-style canvas)) "default")
+                      (let [[left top] (mapv + offset (position event))]
+                        (set! (.-left (.-style canvas)) left)
+                        (set! (.-top (.-style canvas)) top))))))
 
 (defn mouse-up
   [event]
-  (let [{:keys [canvas center drag]} @scene]
-    (when drag
-      (set! (.-cursor (.-style canvas)) "default")
-      (swap! scene assoc
-             :center (mapv + center drag (position event))
-             :drag nil))))
+  (swap! scene dissoc :drag-fn))
 
 (defn mouse-move
   [event]
-  (let [{:keys [canvas center drag]} @scene]
-    (when drag
-      (let [[left top] (mapv + center drag (position event))]
-        (set! (.-left (.-style canvas)) left)
-        (set! (.-top (.-style canvas)) top)))))
+  (let [{:keys [drag-fn]} @scene]
+    (when drag-fn
+      (drag-fn event))))
 
 (defn init!
   []
+  (enable-console-print!)
+  (println "init!")
   (let [viewport (.getElementById js/document "viewport")
         canvas (.getElementById js/document "map")
-        dimensions [(.-clientWidth viewport) (.-clientHeight viewport)]
-        center (mapv (partial * -1) dimensions)]
-    (set! (.-width canvas) (* 3 (first dimensions)))
-    (set! (.-height canvas) (* 3 (last dimensions)))
-    (set! (.-left canvas) (first center))
-    (set! (.-top canvas) (last center))
+        width (.-clientWidth viewport)
+        height (.-clientHeight viewport)]
+    (set! (.-width canvas) (* 3 width))
+    (set! (.-height canvas) (* 3 height))
+    (set! (.-left (.-style canvas)) (* -1 width))
+    (set! (.-top (.-style canvas)) (* -1 height))
     (.addEventListener viewport "mousedown" mouse-down)
     (.addEventListener viewport "mouseup" mouse-up)
     (.addEventListener viewport "mousemove" mouse-move)
     (.addEventListener viewport "mouseout" mouse-up)
-    (swap! scene assoc :canvas canvas :center center)))
+    (swap! scene assoc
+           :canvas canvas
+           :center [0 0]
+           :scale 10)))
 
 (.addEventListener js/window "load" (comp draw! init!))
